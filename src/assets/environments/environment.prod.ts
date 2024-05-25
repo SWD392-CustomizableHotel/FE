e0137d9a4aacdd535a485e428ecd8d2a647b6f5a
Nguyễn Minh Huy <92365053+minhuy2411@users.noreply.github.com>
2024-05-25 20:11:22 +0700

    Update environment.prod.ts
    Add url BE

--- src/assets/environments/environment.prod.ts
+++ src/assets/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    BACKEND_API_URL: 'https://localhost:7082/api'
+    BACKEND_API_URL: 'https://be-sheritonhotel.azurewebsites.net/'
 };
 // Production Server URL, can be replaced into deployed API
