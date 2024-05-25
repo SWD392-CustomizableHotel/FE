e19e21724a4a1cf374c1dfeb5ca5d7d0d1003673
HaiLeVN <hailtse172908@fpt.edu.vn>
2024-05-25 21:51:00 +0700

    chore: remove backslash in env file

--- src/assets/environments/environment.prod.ts
+++ src/assets/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    BACKEND_API_URL: 'https://be-sheritonhotel.azurewebsites.net/'
+    BACKEND_API_URL: 'https://be-sheritonhotel.azurewebsites.net'
 };
 // Production Server URL, can be replaced into deployed API
