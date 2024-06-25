70ba1b947957f1882ae64c0336f3e116ca599c0b
HaiLeVN <lehai112003@gmail.com>
2024-06-25 21:53:08 +0700

    fix: production env for login with gg

--- src/assets/environments/environment.prod.ts
+++ src/assets/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   BACKEND_API_URL: 'https://sheritonhotel.azurewebsites.net',
+  googleClientId: '72251382096-0op13sccaql0p5l7v02k9r16b3n40ule.apps.googleusercontent.com',
 };
 // Production Server URL, can be replaced into deployed API
