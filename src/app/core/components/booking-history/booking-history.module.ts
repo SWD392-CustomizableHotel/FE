7d123a7dd4828ad360cda21b30744e7fab9d5fc9
minhuy2411 <hoanganh241115@gmail.com>
2024-07-07 22:02:17 +0700

    feat: design booking history

--- src/app/core/components/booking-history/booking-history.module.ts
+++ src/app/core/components/booking-history/booking-history.module.ts
@@ -1,30 +1,49 @@
 import { NgModule } from '@angular/core';
-import { PaginatorModule } from 'primeng/paginator';
-import { TableModule } from 'primeng/table';
-import { ButtonModule } from 'primeng/button';
-import { BookingHistoryComponent } from './booking-history.component';
-import { ToastModule } from 'primeng/toast';
-import { DropdownModule } from 'primeng/dropdown';
-import { sharedModule } from '../layout/share/shared.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { ManageAccountsComponent } from './manage-accounts.component';
+// import { AccountsRoutingModule } from './accounts-routing.module';
 import { CommonModule } from '@angular/common';
-
-import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { PaginatorModule } from 'primeng/paginator';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { RatingModule } from 'primeng/rating';
+import { RippleModule } from 'primeng/ripple';
+import { SliderModule } from 'primeng/slider';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { ToolbarModule } from 'primeng/toolbar';
+import { TagModule } from 'primeng/tag';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { BookingHistoryComponent } from './booking-history.component';
+import { sharedModule } from '../layout/share/shared.module';
 
 @NgModule({
   imports: [
     PaginatorModule,
     CommonModule,
-    ToastModule,
-    TableModule,
-    ButtonModule,
-    DropdownModule,
-    sharedModule,
     FormsModule,
-    ReactiveFormsModule,
-    MatDialogModule,
+    TableModule,
+    RatingModule,
+    ButtonModule,
+    SliderModule,
+    InputTextModule,
+    ToggleButtonModule,
+    RippleModule,
+    MultiSelectModule,
+    DropdownModule,
+    ProgressBarModule,
+    ToastModule,
+    ToolbarModule,
     DialogModule,
+    PaginatorModule,
+    TagModule,
+    DynamicDialogModule,
+    sharedModule,
   ],
   declarations: [BookingHistoryComponent],
   providers: [],
