79ab3b597538f30bfa0d7e1022e0e9498d2ee532
tuankiet12012003 <kietdhtse172938@fpt.edu.vn>
2024-07-12 20:17:51 +0700

    feat: web socket

---
+++ src/app/services/web-socket.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Socket, io } from 'socket.io-client';
+import { environment } from '../../assets/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebSocketService {
+  private socket: Socket;
+
+  constructor() {
+    this.socket = io(`${environment.SOCKET_WS}`, {
+        reconnection: true,
+        reconnectionDelay: 1000,
+        reconnectionDelayMax: 5000,
+        reconnectionAttempts: Infinity,
+        transports: ['websocket'] }
+    );
+  }
+
+  public sendMessage(message: string): void {
+    this.socket.emit('message: ', message);
+  }
+
+  public receiveMessage(): void {
+    this.socket.on('message', (message) => {
+      console.log('Received message from server:', message);
+    });
+  }
+  public on(event: string, callback: (data: any) => void): void {
+    this.socket.on(event, callback);
+  }
+
+  public emit(event: string, data: any): void {
+    this.socket.emit(event, data);
+  }
+}
--- src/assets/environments/environment.ts
+++ src/assets/environments/environment.ts
@@ -3,5 +3,6 @@ export const environment = {
     BACKEND_API_URL: 'https://localhost:7082',
     googleClientId: '334776513295-504mhcogp67fv8gju337bcd38jr9d6n1.apps.googleusercontent.com',
     FRONTEND_URL: 'http://localhost:4200',
-    STRIPE_PUBLIC_KEY: 'pk_test_51PZTGERt4Jb0KcASqJvZReNwjK5oiWEzZvp27FpjQ8HadaXWg8WKlKO1kfoIcV7AyuI68Trx4ZdTOne1KPrnNP7e00RBCAu2mn'
+    STRIPE_PUBLIC_KEY: 'pk_test_51PZTGERt4Jb0KcASqJvZReNwjK5oiWEzZvp27FpjQ8HadaXWg8WKlKO1kfoIcV7AyuI68Trx4ZdTOne1KPrnNP7e00RBCAu2mn',
+    SOCKET_WS: 'ws://localhost:8181/'
 };
