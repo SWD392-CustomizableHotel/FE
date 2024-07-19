354745f0f4ff09a6d7064cc9d4d1364dab119b07
HaiLeVN <lehai112003@gmail.com>
2024-07-19 16:22:38 +0700

    fix: environment prod

--- src/assets/environments/environment.prod.ts
+++ src/assets/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   googleClientId: '334776513295-504mhcogp67fv8gju337bcd38jr9d6n1.apps.googleusercontent.com',
   FRONTEND_URL: 'https://fe-customizablehotel.vercel.app',
   STRIPE_PUBLIC_KEY: 'pk_test_51PZTGERt4Jb0KcASqJvZReNwjK5oiWEzZvp27FpjQ8HadaXWg8WKlKO1kfoIcV7AyuI68Trx4ZdTOne1KPrnNP7e00RBCAu2mn',
-  SOCKET_WS: 'ws://sheritonhotel.azurewebsites.net:7082/',
+  SOCKET_WS: 'ws://sheritonhotel.azurewebsites.net:8181/',
   CONFIRM_URL: 'https://fe-customizablehotel.vercel.app/confirm-payment'
 };
 // Production Server URL, can be replaced into deployed API
